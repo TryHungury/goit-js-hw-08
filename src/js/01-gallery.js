1d73e7cad34ea0d63617aaac0dd8060506b971ac
TryHungury <mukolanadebernuy@gmail.com>
2022-10-30 12:28:06 +0200

    task 1 complete

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import "simplelightbox/dist/simple-lightbox.min.css";
+import "simplelightbox";
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery')
+
+
+const makeItemMarkup = (galleryItems) => {
+    const imgMarkup = galleryItems.map(({preview, original, description})=>{
+      return `
+      <a class = gallery__item href = "${original}">
+      <img src="${preview}" alt="${description}" data-source="${original}" class = gallery__image>
+      </img></a>` 
+    })
+    // console.log("🚀liMarkup", imgMarkup)
+    
+    return imgMarkup;
+}
+
+galleryRef.insertAdjacentHTML('afterbegin', makeItemMarkup(galleryItems).join(''))
+
+const clickOnImg = (e) => {
+    e.preventDefault();
+    
+    if(e.target.nodeName !== 'IMG') {
+        return;
+    }
+}
+
+galleryRef.addEventListener('click', clickOnImg);
+new SimpleLightbox('.gallery a', {animationSpeed: 400, captionsData: 'alt',captionDelay: 250});
+
+// console.log(galleryItems);
+
