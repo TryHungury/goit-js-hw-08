541c1ea76dbb2df4df84248bfba9eab608ce083c
TryHungury <mukolanadebernuy@gmail.com>
2022-10-30 13:37:20 +0200

    task 2 complete

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import "lodash.throttle"
+import Player from '@vimeo/player';
+import throttle from "lodash.throttle";
 
+const KEY_STORAGE_TIME = "videoplayer-current-time";
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+// console.log(player)
+// console.log(iframe)
+
+const onPlay = function(e) {
+    localStorage.setItem(KEY_STORAGE_TIME, e.seconds)
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(KEY_STORAGE_TIME)));
